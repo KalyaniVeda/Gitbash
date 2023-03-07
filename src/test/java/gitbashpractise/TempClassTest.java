cd21e412710c6109c6eebfb419f334515dc17f00
KalyaniVeda <kalyani.veda@gmail.com>
2023-03-07 14:15:11 +0530

    FirstCommit

---
+++ src/test/java/gitbashpractise/TempClassTest.java
@@ -0,0 +1,11 @@
+package gitbashpractise;
+
+import org.testng.annotations.Test;
+
+public class TempClassTest {
+@Test
+	public void tempMethod()
+	{
+		System.out.println("Temporary method");
+	}
+}
